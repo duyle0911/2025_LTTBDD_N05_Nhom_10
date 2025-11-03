c25d7cbe37a743a825e6df90d2b23b6aea300f1b
duyle0911 <duyle09112005@gmail.com>
2025-11-03 21:38:06 +0700

    update

--- appquanlychitieucanhan/lib/main.dart
+++ appquanlychitieucanhan/lib/main.dart
@@ -1,1 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'l10n/app_localizations.dart';
+import 'l10n/l10n_ext.dart';
+
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const seed = Color(0xFF0A84FF);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateTitle: (ctx) => ctx.l10n.appTitle,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: seed, brightness: Brightness.light),
+        scaffoldBackgroundColor: const Color(0xFFF7F9FC),
+        splashFactory: InkSparkle.splashFactory,
+        appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const LoginScreen(),
+        '/register': (_) => const RegisterScreen(),
+        '/home': (_) => const HomePlaceholder(),
+      },
+    );
+  }
+}
+
+class HomePlaceholder extends StatelessWidget {
+  const HomePlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final t = context.l10n;
+    return Scaffold(
+      appBar: AppBar(title: Text(t.appTitle)),
+      body: const Center(
+        child: Text('Logged in! This is /home (placeholder screen)'),
+      ),
+    );
+  }
+}
